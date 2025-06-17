c786c5865399b1574199ddd17e098bf24783cdc4
AlievaAA <AlievaAA13@str.mos.ru>
2025-06-17 11:14:20 +0300

    rewrite classes Text and Paragraph

--- core/doc_objects/Paragraph.py
+++ core/doc_objects/Paragraph.py
@@ -1,10 +1,12 @@
 import random
 from typing import overload
-
+from typing import Optional
+from docx.parts.story import StoryPart
 from docx.text.paragraph import Paragraph
 from docx.oxml import parse_xml
-
-from core.doc_objects.styles import ParagraphStyle
+from core.doc_objects.Text import Text
+from core.styles.stylist import set_style
+from core.styles.paragraph import ParagraphStyle
 
 
 class DOCParagraph(Paragraph):
@@ -13,50 +15,29 @@ class DOCParagraph(Paragraph):
     """
 
     @overload
-    def __init__(self, paragraph: Paragraph):
-        ...
+    def __init__(self, paragraph: Paragraph): ...
 
     @overload
-    def __init__(self, paragraph: Paragraph, style: ParagraphStyle):
-        ...
+    def __init__(self, paragraph: Paragraph, linked_objects: list): ...
 
     @overload
-    def __init__(self, paragraph: Paragraph, linked_objects: list):
-        ...
+    def __init__(self): ...
 
-    @overload
-    def __init__(self, paragraph: Paragraph, style: ParagraphStyle, linked_objects: list):
-        ...
-
-    @overload
-    def __init__(self):
-        ...
-
-    def __init__(self, *args):
-        if not args:
-            from docx.parts.story import StoryPart
-            def_p = self._create_default_paragraph()
-            super().__init__(def_p, StoryPart.part)
+    def __init__(self, paragraph: Optional[Paragraph] = None, linked_objects: Optional[list] = None):
+        if paragraph is None:
+            xml = self._create_default_p()
+            super().__init__(xml, StoryPart.part)
         else:
-            source = args[0]
-            style = None
             linked_objects = None
-            if len(args) == 2:
-                if isinstance(args[1], list):
-                    linked_objects = args[0]
-                elif isinstance(args[1], ParagraphStyle):
-                    style = args[1]
-            elif len(args) == 3:
-                linked_objects = args[0]
-                style = args[1]
-            if isinstance(source, Paragraph):
-                super().__init__(source._p)
+            if isinstance(paragraph, Paragraph):
+                super().__init__(paragraph._p)
                 self._linked_objects = linked_objects
-                self._style = style
             else:
                 raise AttributeError (f"Creating Paragraph object failed:"
-                                      f"Unknown source {type(source)}!")
-
+                                      f"Unknown source {type(paragraph)}!")
+            
+    def add_run(self, text: Text):
+        return super().add_run(text)
 
     @staticmethod
     def _create_default_paragraph():
@@ -88,7 +69,7 @@ class DOCParagraph(Paragraph):
         self._linked_objects = new
 
     @staticmethod
-    def _gen_random_paragraph_id(length:int = 8):
+    def _gen_random_paragraph_id(length:int = 8) -> str:
         """Generate random id for attributes in paragraph"""
         return f"{random.getrandbits(32 * length):0{length}x}"
 
@@ -97,3 +78,6 @@ class DOCParagraph(Paragraph):
     
     def __repr__(self):
         return self.__str__()
+    
+    def add_style(self, dc_style: ParagraphStyle):
+        set_style(self._r, dc_style)
--- core/doc_objects/Text.py
+++ core/doc_objects/Text.py
@@ -1,34 +1,76 @@
-from typing import overload, Union, cast
+from typing import cast
+from typing import Union
+from typing import overload
 from collections import UserString
-from docx.oxml import parse_xml
-from docx.text.run import Run
 
-from core.styles.stylist import set_style
+from docx.text.run import Run
+from docx.oxml import parse_xml
+from docx.oxml.text.run import CT_R
+from docx.parts.story import StoryPart
+
 from core.styles.text import TextStyle
+from core.styles.stylist import set_style
 
 
 class Text(UserString, Run):
+    """
+        A class representing formatted text in a document.
+    """
 
     @overload
-    def __init__(self, text: Union [str| Run]):
+    def __init__(self):
         ...
 
     @overload
-    def __init__(self, text: Union [str| Run], linked_objects: list):
+    def __init__(self, text: Union[str, UserString, Run]):
         ...
 
-    def _create_default_run_pr(self):
+    @overload
+    def __init__(self, text: Union[str, UserString, Run], linked_objects: list):
+        ...
+
+    def __init__(self, *args):
+        if not args:
+            xml = self._create_default_run_pr()
+            Run().__init__(xml, StoryPart.part)
+        else:
+            source = args[0]
+            linked_objects = None
+            if len(args) == 2:
+                linked_objects = args[1]
+            if isinstance(source, Run):
+                Run().__init__(source._r)
+                self._linked_objects = linked_objects
+            elif isinstance(source, str) or isinstance(source, UserString):
+                xml = self._create_run_pr_with_text(source)
+                Run().__init__(xml, StoryPart.part)
+                self._linked_objects = linked_objects
+            else:
+                raise AttributeError(f"Creating Text object failed:"
+                                     f"Unknown source {type(source)}!")
+
+    def _create_run_pr_with_text(self, text: str) -> CT_R:
         r = parse_xml(
             '<w:r>'
-                '<w:rPr>'
-                    '<w:lang w:val="en-US"/>'
-                '</w:rPr>'
-                '<w:t> xml:space="preserve"> </w:t>'
+            '<w:rPr>'
+            '<w:lang w:val="ru-RU"/>'
+            '</w:rPr>'
+            f'<w:t xml:space="preserve">{text}</w:t>'
             '</w:r>'
         )
-        return cast()
-        
+        return cast("CT_R", r)
 
+    @staticmethod
+    def _create_default_run_pr() -> CT_R:
+        r = parse_xml(
+            '<w:r>'
+            '<w:rPr>'
+            '<w:lang w:val="ru-RU"/>'
+            '</w:rPr>'
+            '<w:t xml:space="preserve"> </w:t>'
+            '</w:r>'
+        )
+        return cast("CT_R", r)
 
     @property
     def linked_objects(self) -> list:
@@ -40,23 +82,9 @@ class Text(UserString, Run):
 
     def __str__(self):
         return "<DOC.TEXT object>"
-    
+
     def __repr__(self):
         return self.__str__()
-    
+
     def add_style(self, dc_style: TextStyle):
-        set_style()
-
-
-    # @property
-    # def text(self):
-    #     return self.data
-
-    # @text.setter
-    # def text(self, value: str):
-    #     if not isinstance(value, str):
-    #         raise AttributeError(f"value must be str not {type(value)}!")
-    #     self.data = value
-
-
-t = Text(text="hello")
+        set_style(self._r, dc_style)
