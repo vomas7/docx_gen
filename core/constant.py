403275fa07ccfe8048a9e6dbe4c47af3a46b7cc3
vomas7 <superjob214@yandex.ru>
2025-06-20 12:53:40 +0300

    fix paragraph

--- core/constant.py
+++ core/constant.py
@@ -12,3 +12,23 @@ SECTION_STANDARD = ('<w:sectPr xmlns:w="http://schemas.openxmlformats.org'
                     '  <w:cols w:space="720"/>'
                     '  <w:docGrid w:linePitch="360"/>'
                     '</w:sectPr>')
+PARAGRAPH_STANDARD = ("""
+            <w:p 
+                xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"
+                xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml"
+                w14:paraId="{random_paragraph_id}" 
+                w14:textId="{random_paragraph_id}" 
+                w:rsidR="{random_paragraph_id}" 
+                w:rsidRPr="{random_paragraph_id}" 
+                w:rsidRDefault="{random_paragraph_id}" 
+                w:rsidP="{random_paragraph_id}"
+            >
+                <w:pPr>
+                    <w:rPr/>
+                </w:pPr>
+                <w:r>
+                    <w:rPr/>
+                    <w:t></w:t>
+                </w:r>
+            </w:p>
+            """)
