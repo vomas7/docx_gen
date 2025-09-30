dcf32ac18d72d351d1ec38e728af61edd51e986f
AkentevDV <@akentevdv>
2025-09-30 14:41:11 +0300

    tmp

--- core/t.py
+++ core/t.py
@@ -1,32 +1,67 @@
-import copy
+import os
+import sys
+from types import UnionType
 
-from docx.oxml import OxmlElement
-from docx.oxml.ns import qn
+current_path = os.getcwd()
+root_path = os.path.abspath(os.path.join(current_path, ".."))
+sys.path.append(root_path)
 
 from core.doc import DOC
 from core.doc_objects.Section import DOCSection
-from core.styles.section import SectionStyle
-from core.styles.paragraph import ParagraphStyle
+from core.doc_objects.Paragraph import DOCParagraph
+from core.doc_objects.Text import Text
+from docx.text.run import Run
+from docx.text.paragraph import Paragraph
+from docx.parts.story import StoryPart
+
+# adder (add_x) - принимает в себя аргументы и записывает их внуть добавляемого элемента add_t(text = 'text') - Добавит внутрь _Text() атрибут text со значение "text"
+# Также сам pydocs внутри метакласса формирует у объектов - xml на основе self.append и self.appprevios (::_EtreeXML либы)
+
+
+#TODO создать генератор xml для всех элементов!
+
+#TODO проблема заключается в том, что мы не не можем добавлять свои объекты в итоговый документ, по-факту происходит изменение текстов и стилей существующих.
+#TODO т.е из-за того что не создаются собственные элементы, не получается добавлять их в документ иерархически - путается порядок и нужно коньролировать каждый объект на его добавление.
+
+cast_doc = DOC()
+
+
+t1=Text("text")
+t2=Text()
+
+p1 = DOCParagraph(t1)
+p2 = DOCParagraph(t2)
+
+cast_doc.writer.add_paragraph(p1)
+cast_doc.writer.add_paragraph(p2)
+print(p1._element.xml)
+
+# print(cast_doc._element.xml)
 
 
 
-d = DOC()
-ParagraphStyle().alignment = 10
-s1 = DOCSection()
-s1.add_style(SectionStyle(right_margin=0))
-s2 = DOCSection()
-s2.add_style(SectionStyle(right_margin=5))
-s3 = DOCSection()
-s3.add_style(SectionStyle(right_margin=10))
-d.writer.add_section(s3, -1)
 
-d.writer.add_section(s2, 0)
-d.writer.add_paragraph()
+# p = cast_doc.add_paragraph("ffff")
+# run = Text("some1\n\tdfdfdf")
+#
+# par1 = DOCParagraph(p)
 
-# d.writer.add_section(s1, -1)
-d.writer.replace_section(s1)
-print(d._element.body.xml)
-d.export.to_docx('test.docx')
+# print(par1._element.xml)
+
+
+
+
+
+# print([se.start_type for se in sections])
+# print(cast_doc._body._element.xml)
+# cast_doc.save("test.docx")
+
+
+
+
+
+
+
 
 # ('_pgMar', <class 'docx.oxml.section.CT_PageMar'>), ('_pgSz', <class 'docx.oxml.section.CT_PageSz'>), ('_titlePg', <class 'docx.oxml.shared.CT_OnOff'>), ('_scType', <class 'docx.oxml.section.CT_SectType'>)]
 # {'w:pgSz': <CT_PageSz '<w:pgSz>' at 0x1f6015ec2d0>, 'w:pgMar': <CT_PageMar '<w:pgMar>' at 0x1f6015ecf50>}
