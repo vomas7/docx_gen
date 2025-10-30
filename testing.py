65a0a5c83307db6f49f896e0377e0f72241c6ee7
AkentevDV <@akentevdv>
2025-10-30 13:14:36 +0300

    renew docx_gen comeback

--- testing.py
+++
@@ -1,9 +0,0 @@
-from core.doc_objects.Paragraph import DOCParagraph
-from core.doc_objects.Text import Text
-from core.styles.text import TextStyle
-from core.styles.paragraph import ParagraphStyle
-from core.doc import DOC
-
-doc = DOC()
-text = Text("привет-привет")
-pr = DOCParagraph()
