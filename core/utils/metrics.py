9f7374271bbdca99df3ce2742338cbb8381d379b
Trydimas <akentev.dima@bk.ru>
2026-01-19 13:32:36 +0300

    add ci github actions

--- core/utils/metrics.py
+++ core/utils/metrics.py
@@ -109,9 +109,9 @@ class RGBColor(tuple[int, int, int]):
         msg = "RGBColor() takes three integer values 0-255"
         for val in (r, g, b):
             if (
-                not isinstance(val, int)  # pyright: ignore[reportUnnecessaryIsInstance]
-                or val < 0
-                or val > 255
+                    not isinstance(val, int)  # pyright: ignore[reportUnnecessaryIsInstance]
+                    or val < 0
+                    or val > 255
             ):
                 raise ValueError(msg)
         return super(RGBColor, cls).__new__(cls, (r, g, b))
