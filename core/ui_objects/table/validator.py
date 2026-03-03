a4185e5262f17196349c9a00221d91e79d020d8a
spacewalrus73 <spacewalrus@mail.ru>
2026-03-03 16:01:28 +0300

    add handle of NoneType

--- core/ui_objects/table/validator.py
+++ core/ui_objects/table/validator.py
@@ -1,14 +1,21 @@
+from types import NoneType
+
 TABLE_COLUMNS_MAX_COUNT = 64
 
 
 def validate_word_table_rows(rows_count: int) -> None | ValueError:
-    validate_positive_integer_number(rows_count)
+    if isinstance(rows_count, NoneType):
+        return None
+    return validate_positive_integer_number(rows_count)
 
 
 def validate_word_table_columns(columns_count: int) -> None | ValueError:
+    if isinstance(columns_count, NoneType):
+        return None
     validate_positive_integer_number(columns_count)
     if not is_less_than_max_columns(columns_count):
         raise ValueError(f"Num of columns must be less than {TABLE_COLUMNS_MAX_COUNT}")
+    return None
 
 
 def validate_positive_integer_number(number: int) -> None | ValueError:
